0abd49819e0093cbfd71a76eac5a179a512e6f7b
justin <justin.ad.park@gmail.com>
2019-03-17 22:02:25 +0900

    Adding node server troposhere script

--- nodeserver-cf-template.py
+++ nodeserver-cf-template.py
@@ -97,6 +97,19 @@ t.add_resource(Role(
     )
 ))
 
+t.add_resource(IAMPolicy(
+    "Policy",
+    PolicyName="AllowS3",
+    PolicyDocument=Policy(
+        Statement=[
+            Effect=Allow,
+            Action=[Action("s3","*")],
+            Resource=[*])
+        ]
+    ),
+    Roles=[Ref("Role")]
+))
+
 t.add_resource(InstanceProfile(
     "InstanceProfile",
     Path="/",
